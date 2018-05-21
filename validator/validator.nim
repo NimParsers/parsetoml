3668dcfca409190735299aff50ad7819e36c5599
data-man <datamanrb@gmail.com>
2018-05-21 20:48:10 +0300

    Update the validator

--- validator/validator.nim
+++ validator/validator.nim
@@ -1,7 +1,23 @@
 import parsetoml
 import json
 import streams
+import os
 
-let table = parsetoml.parseStream(newFileStream(stdin))
+var
+  fs: FileStream
+
+if paramCount() > 0:
+  fs = newFileStream(paramStr(1), bufSize = 32768)
+else:
+  fs = newFileStream(stdin)
+
+if fs.isNil:
+  echo "Error: '", paramStr(1), "' can't be opened!"
+  quit(QuitFailure)
+
+let table = parsetoml.parseStream(fs)
+
+fs.close
 
 echo table.toJson.pretty
+
