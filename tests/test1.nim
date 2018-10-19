f976e86dd7531f574e57a38c78aefd70d9273379
Matt Haggard <haggardii@gmail.com>
2018-10-19 13:19:22 -0600

    The start of some tests

---
+++ tests/test1.nim
@@ -0,0 +1,11 @@
+import macros
+import unittest
+import parsetoml
+import typetraits
+
+test "parseString":
+  let table1 = parsetoml.parseString(""""
+[input]
+file_name = "test.txt"
+""")
+  check table1["input"]["file_name"] == "test.txt"
